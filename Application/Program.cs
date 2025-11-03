161ae57fb9d3a1bab853d259aba417e0fab6d96e
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2025-11-03 18:00:20 +0530

    refactor: Convert project to a solution
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Application;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var keypad = new Keypad.OldPhonePad();
+
+        Console.WriteLine($"222 2 22#: {keypad.process("222 2 22#")}"); //=> output: CAB
+        Console.WriteLine($"33#: {keypad.process("33#")}"); //=> output: E
+        Console.WriteLine($"227 *#: {keypad.process("227 *#")}"); //=> output: B
+        Console.WriteLine($"4433555 555666#: {keypad.process("4433555 555666#")}"); //=> output: HELLO
+        Console.WriteLine($"8 88777444666 * 664#: {keypad.process("8 88777444666 * 664#")}"); //=> output: ?????
+    }
+}
--- Keypad/OldPhonePad.cs
+++ Keypad/OldPhonePad.cs
@@ -1,10 +1,8 @@
-﻿using System.Text;
+﻿namespace Keypad;
 
-namespace keypad;
-
-class Program
+public class OldPhonePad
 {
-    static Dictionary<char, string> Keys = new Dictionary<char, string>
+    private Dictionary<char, string> Keys = new Dictionary<char, string>
     {
         { '1', "&\'(" },
         { '2', "abc" },
@@ -20,18 +18,9 @@ class Program
         { '#', "" }
     };
 
-    static void Main(string[] args)
+    public string process(string input)
     {
-        Console.WriteLine($"222 2 22#: {OldPhonePad("222 2 22#")}"); //=> output: CAB
-        Console.WriteLine($"33#: {OldPhonePad("33#")}"); //=> output: E
-        Console.WriteLine($"227 *#: {OldPhonePad("227 *#")}"); //=> output: B
-        Console.WriteLine($"4433555 555666#: {OldPhonePad("4433555 555666#")}"); //=> output: HELLO
-        Console.WriteLine($"8 88777444666 * 664#: {OldPhonePad("8 88777444666 * 664#")}"); //=> output: ?????
-    }
-
-    static string OldPhonePad(string input)
-    {
-        var result = new StringBuilder();
+        var result = new System.Text.StringBuilder();
         int frequency = 0;
 
         for (int i = 1; i < input.Length; i++)

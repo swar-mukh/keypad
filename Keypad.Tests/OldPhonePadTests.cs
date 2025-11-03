c001a2dcba7455be818f347bcb1c6c7ef3599d8c
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2025-11-03 18:33:14 +0530

    feat: Add tests
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

---
+++ Keypad.Tests/OldPhonePadTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Keypad.Tests;
+
+public class OldPhonePadTests
+{
+    [Theory]
+    [InlineData("222 2 22#", "cab")]
+    [InlineData("33#", "e")]
+    [InlineData("227 *#", "bp\b")]
+    [InlineData("4433555 555666#", "hello")]
+    [InlineData("8 88777444666 * 664#", "turio\bng")]
+    public void OldPhonePadTheory(string input, string expectedResult)
+    {
+        var _oldPhonePad = new Keypad.OldPhonePad();
+
+        string result = _oldPhonePad.process(input);
+
+        Assert.Equal(expectedResult, result);
+    }
+}

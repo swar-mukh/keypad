74bd5d683c8141d10370ab677137460aabcae9a0
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2025-11-04 15:57:22 +0530

    feat: Add tests for T9 input method
    Signed-off-by: Swarnava Mukherjee <swar_mukh10@yahoo.co.in>

---
+++ Keypad.Tests/T9InputTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Keypad.Tests;
+
+public class T9InputTests
+{
+    [Theory]
+    [InlineData("123", "??")]
+    [InlineData("84265 968", "thank you")]
+    [InlineData("43556 2629464 5433", "hello amazing life")]
+    public void T9InputTheory(string input, string expectedResult)
+    {
+        var _t9Input = new T9Input();
+
+        string result = _t9Input.Process(input);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
